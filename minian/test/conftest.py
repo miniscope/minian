732b99e026ee6ef76eebbdcb34ca9c55856fe4f4
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-05-27 18:47:26 -0700

    move env vars to pytest

---
+++ minian/test/conftest.py
@@ -0,0 +1,12 @@
+import os
+
+import psutil
+
+
+def pytest_sessionstart(session):
+    """Set env vars for dask resource limits"""
+    memory = psutil.virtual_memory()
+    total_gb = memory.total / (2**30)
+    os.environ["MINIAN_NWORKERS"] = "1"
+    os.environ["MINIAN_MEM_LIMIT"] = f"{total_gb * .75:.2f}GB"
+    os.environ["MINIAN_INTERACTIVE"] = "False"
